a4d438dd3d4ab7b8b4d41d4789c4d6e28464557b
Bruce <bruceunqo@gmail.com>
2026-03-24 14:51:15 +1100

    Add console.log statements for personal information

---
+++ index.js
@@ -0,0 +1,20 @@
+/* Use console.log to output your full name to the console */
+console.log('Bruce Wanjiru');
+
+/* Output your year of birth. */
+console.log('2008');
+
+/* Try again to output your year of birth, this time passing the date without the quotes. */
+console.log(2008);
+
+/* We can pass several arguments to console.log separated by commas */
+console.log("Bruce", "Wanjiru", "2008");
+
+/* Output the same information (name, surname, year) to the console, not side by side, but on consecutive lines. */
+console.log('Bruce');
+console.log('Wanjiru');
+console.log(2008);
+
+/* Try writing your name, surname, and year of birth on one line again, this time not separated by commas, but by + signs. */
+console.log("Bruce" + "Wanjiru" + "(2008)");
+
